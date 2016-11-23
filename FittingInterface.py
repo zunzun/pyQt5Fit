6c1d5aec1c07f1909928c4717e2a5cae6bfbc52c
zunzun <zunzun@zunzun.com>
2016-11-23 10:29:35 -0600

    Changed equation selection from radiobuttons to combobox

--- FittingInterface.py
+++ FittingInterface.py
@@ -1,5 +1,7 @@
-import os, sys, queue, pickle, time
+import os, sys, queue, pickle, time, inspect
 import pyeq3
+
+import matplotlib # ensure this dependency imports for later use in fitting results
 
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
@@ -73,51 +75,56 @@ class InterfaceWindow(QWidget):
         # ROW 4 - equation selection labels
         # no "self" needed as no later references exist
         row, col = (4, 1)
-        l = QLabel("<b>--- Example 2D Equations ---</b>") # bold tags
+        l = QLabel("<b>--- Standard 2D Equations ---</b>") # bold tags
         grid.addWidget(l, row, col)
         
         row, col = (4, 3)
-        l = QLabel("<b>--- Example 3D Equations ---</b>") # bold tags
+        l = QLabel("<b>--- Standard 3D Equations ---</b>") # bold tags
         grid.addWidget(l, row, col)
 
-        # ROW 5 - equation selection radio buttons
+        # ROW 5 - equation selection
         row, col = (5, 1)
-        self.eqSelectButtonGroup_2D = QButtonGroup()
-        w = QWidget()
+        self.cb_Modules2D = QComboBox(self)
+        self.cb_Modules2D.activated.connect(self.moduleSelectChanged_2D)
+        moduleNameList = list(dfc.eq_od2D.keys())
+        moduleNameList.remove('Rational') # has no 2D equations used here
+        self.cb_Modules2D.addItems(moduleNameList)
+        self.cb_Modules2D.setCurrentIndex(moduleNameList.index('Polynomial'))
+        
+        self.cb_Equations2D = QComboBox(self)
+        equationNameList = self.GetEquationListForModule(2, 'Polynomial')
+        self.cb_Equations2D.addItems(equationNameList)
+        self.cb_Equations2D.setCurrentIndex(equationNameList.index('1st Order (Linear)'))
+
         vBox = QVBoxLayout()
         vBox.setSpacing(0)
         vBox.setContentsMargins(0,0,0,0)
+        vBox.addWidget(self.cb_Modules2D)
+        vBox.addWidget(self.cb_Equations2D)
+        w = QWidget()
         w.setLayout(vBox)
         grid.addWidget(w, row, col)
-
-        index=0
-        for exampleEquationText in dfc.eq_od2D.keys():
-            rb = QRadioButton(exampleEquationText)
-            rb.toggled.connect(lambda:self.onEquationSelect_2D())
-            if index == 0:
-                rb.setChecked(True)
-            vBox.addWidget(rb)
-            self.eqSelectButtonGroup_2D.addButton(rb, index)
-            index += 1
             
         row, col = (5, 3)
-        self.eqSelectButtonGroup_3D = QButtonGroup()
-        w = QWidget()
+        self.cb_Modules3D = QComboBox(self)
+        self.cb_Modules3D.activated.connect(self.moduleSelectChanged_3D)
+        moduleNameList = list(dfc.eq_od3D.keys())
+        self.cb_Modules3D.addItems(moduleNameList)
+        self.cb_Modules3D.setCurrentIndex(moduleNameList.index('Polynomial'))
+        
+        self.cb_Equations3D = QComboBox(self)
+        equationNameList = self.GetEquationListForModule(3, 'Polynomial')
+        self.cb_Equations3D.addItems(equationNameList)
+        self.cb_Equations3D.setCurrentIndex(equationNameList.index('Linear'))
+
         vBox = QVBoxLayout()
         vBox.setSpacing(0)
         vBox.setContentsMargins(0,0,0,0)
+        vBox.addWidget(self.cb_Modules3D)
+        vBox.addWidget(self.cb_Equations3D)
+        w = QWidget()
         w.setLayout(vBox)
         grid.addWidget(w, row, col)
-
-        index=0
-        for exampleEquationText in dfc.eq_od3D.keys():
-            rb = QRadioButton(exampleEquationText)
-            rb.toggled.connect(lambda:self.onEquationSelect_3D())
-            if index == 0:
-                rb.setChecked(True)
-            vBox.addWidget(rb)
-            self.eqSelectButtonGroup_3D.addButton(rb, index)
-            index += 1
 
         # ROW 6 - empty label as visual buffer
         row, col = (6, 0)
@@ -195,16 +202,69 @@ class InterfaceWindow(QWidget):
                   (resolution.height() // 2) - (self.frameSize().height() // 2))
 
 
+    def GetEquationListForModule(self, inDimension, inModuleName):
+        strModule = 'pyeq3.Models_' + str(inDimension) + 'D.' + inModuleName
+        moduleMembers = inspect.getmembers(eval(strModule))
+        returnList = []
+        for equationClass in moduleMembers:
+            if inspect.isclass(equationClass[1]):
+                for extendedVersionName in ['Default', 'Offset']:
+                    
+                    # if the equation *already* has an offset,
+                    # do not add an offset version here
+                    if (-1 != extendedVersionName.find('Offset')) and (equationClass[1].autoGenerateOffsetForm == False):
+                        continue
+                        
+                    # in this application, exclude equation than need extra input
+                    if equationClass[1].splineFlag or \
+                            equationClass[1].userSelectablePolynomialFlag or \
+                            equationClass[1].userCustomizablePolynomialFlag or \
+                            equationClass[1].userSelectablePolyfunctionalFlag or \
+                            equationClass[1].userSelectableRationalFlag or \
+                            equationClass[1].userDefinedFunctionFlag:
+                        continue
+
+                    equation = equationClass[1]('SSQABS', extendedVersionName)
+
+                    returnList.append(equation.GetDisplayName())
+
+        returnList.sort()
+        return returnList
+
+
+    def moduleSelectChanged_2D(self):
+        listIndex = self.cb_Modules2D.currentIndex()
+        moduleName = list(dfc.eq_od2D.keys())[listIndex]
+        equationNameList = self.GetEquationListForModule(2, moduleName)
+        self.cb_Equations2D.clear()
+        self.cb_Equations2D.addItems(equationNameList)
+        self.cb_Equations2D.setCurrentIndex(0)
+
+
+    def moduleSelectChanged_3D(self):
+        listIndex = self.cb_Modules3D.currentIndex()
+        moduleName = list(dfc.eq_od3D.keys())[listIndex]
+        equationNameList = self.GetEquationListForModule(3, moduleName)
+        self.cb_Equations3D.clear()
+        self.cb_Equations3D.addItems(equationNameList)
+        self.cb_Equations3D.setCurrentIndex(0)
+
+
     def onFit_2D(self):
         textData = self.text_2D.toPlainText()
-        equationSelection = list(dfc.eq_od2D.keys())[self.equationSelect_2D]
-        fittingTargetSelection = dfc.fittingTargetList[self.fittingTargetSelect_2D]
+
+        moduleListIndex = self.cb_Modules2D.currentIndex()
+        moduleName = list(dfc.eq_od2D.keys())[moduleListIndex]
+        equationListIndex = self.cb_Equations2D.currentIndex()
+        equationNameList = self.GetEquationListForModule(2, moduleName)
+        equationName = equationNameList[equationListIndex]
 
         # the GUI's fitting target string contains what we need - extract it
+        fittingTargetSelection = dfc.fittingTargetList[self.fittingTargetSelect_2D]
         fittingTarget = fittingTargetSelection.split('(')[1].split(')')[0]
 
-        item = dfc.eq_od2D[equationSelection]
-        eqString = 'pyeq3.Models_2D.' + item[0] + '(fittingTarget, ' + "'" + item[1] + "'" + item[2] + ')'
+        item = dfc.eq_od2D[moduleName][equationName]
+        eqString = 'pyeq3.Models_2D.' + moduleName + '.' + item[0] + "('" + fittingTarget + "','" + item[1] + "')"
         self.equation = eval(eqString)
 
         # convert text to numeric data checking for log of negative numbers, etc.
@@ -238,14 +298,19 @@ class InterfaceWindow(QWidget):
 
     def onFit_3D(self):
         textData = self.text_3D.toPlainText()
-        equationSelection = list(dfc.eq_od3D.keys())[self.equationSelect_3D]
-        fittingTargetSelection = dfc.fittingTargetList[self.fittingTargetSelect_3D]
+
+        moduleListIndex = self.cb_Modules3D.currentIndex()
+        moduleName = list(dfc.eq_od3D.keys())[moduleListIndex]
+        equationListIndex = self.cb_Equations3D.currentIndex()
+        equationNameList = self.GetEquationListForModule(3, moduleName)
+        equationName = equationNameList[equationListIndex]
 
         # the GUI's fitting target string contains what we need - extract it
+        fittingTargetSelection = dfc.fittingTargetList[self.fittingTargetSelect_3D]
         fittingTarget = fittingTargetSelection.split('(')[1].split(')')[0]
 
-        item = dfc.eq_od3D[equationSelection]
-        eqString = 'pyeq3.Models_3D.' + item[0] + '(fittingTarget, ' + "'" + item[1] + "'" + item[2] + ')'
+        item = dfc.eq_od3D[moduleName][equationName]
+        eqString = 'pyeq3.Models_3D.' + moduleName + '.' + item[0] + "('" + fittingTarget + "','" + item[1] + "')"
         self.equation = eval(eqString)
 
         # convert text to numeric data checking for log of negative numbers, etc.
